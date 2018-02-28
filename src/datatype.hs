33b4a573c4f19375f87088d0935feb02a4f9015f
Gaufoo <zhongzc_arch@outlook.com>
2018-02-28 15:01:50 +0800

    add Exponment/RealNum/RationalNum

---
+++ src/datatype.hs
@@ -0,0 +1,11 @@
+module Fscheme.Types 
+  ( LispVal(..) )
+where
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+             | Character Char
